16edd1e597227eb2aa4dcfb04b6e8a9f4df77fa5
luke sadler <mylogon@gmail.com>
2024-05-19 09:42:14 +0100

    message cache

--- server.js
+++ server.js
@@ -8,41 +8,43 @@ app.use(express.static(html))
 const port = process.argv[2] || 8090;
 const http = require("http").Server(app)
 const io = require("socket.io")(http);
+let messageCache = [];
+let cache_size = process.env.CACHE_SIZE ?? 20
 
 http.listen(port, function () {
-  console.log("Starting server on port %s", port);
+	console.log("Starting server on port %s", port);
 });
 
 const users = [];
 let msg_id = 1;
-io.sockets.on("connection", function(socket) {
+io.sockets.on("connection", function (socket) {
 	console.log("New connection!");
 
 	var nick = null;
 
-	socket.on("login", function(data) {
+	socket.on("login", function (data) {
 		// Security checks
 		data.nick = data.nick.trim();
 
 		// If is empty
-		if(data.nick == ""){
+		if (data.nick == "") {
 			socket.emit("force-login", "Nick can't be empty.");
 			nick = null;
-			return ;
+			return;
 		}
 
 		// If is already in
-		if(users.indexOf(data.nick) != -1){
+		if (users.indexOf(data.nick) != -1) {
 			socket.emit("force-login", "This nick is already in chat.");
 			nick = null;
-			return ;
+			return;
 		}
 
 		// Save nick
 		nick = data.nick;
 		users.push(data.nick);
 
-		console.log("User %s joined.", nick.replace(/(<([^>]+)>)/ig,""));
+		console.log("User %s joined.", nick.replace(/(<([^>]+)>)/ig, ""));
 		socket.join("main");
 
 		// Tell everyone, that user joined
@@ -54,41 +56,54 @@ io.sockets.on("connection", function(socket) {
 		socket.emit("start", {
 			"users": users
 		});
+
+		// Send the message cache to the new user
+		console.log(`going to send cache to ${nick}`)
+		socket.emit("previous-msg", {
+			"msgs": messageCache
+		});
 	});
 
-	socket.on("send-msg", function(data){
+	socket.on("send-msg", function (data) {
 		// If is logged in
-		if(nick == null){
+		if (nick == null) {
 			socket.emit("force-login", "You need to be logged in to send message.");
-			return ;
+			return;
+		}
+
+		const msg = {
+			"f": nick,
+			"m": data.m,
+			"id": "msg_" + (msg_id++)
+		}
+
+		messageCache.push(msg);
+		if (messageCache.length > cache_size) {
+			messageCache.shift(); // Remove the oldest message
 		}
 
 		// Send everyone message
-		io.to("main").emit("new-msg", {
-			"f": nick,
-			"m": data.m,
-			"id": "msg_"+(msg_id++)
-		});
+		io.to("main").emit("new-msg", msg);
 
-		console.log("User %s sent message.", nick.replace(/(<([^>]+)>)/ig,""));
+		console.log("User %s sent message.", nick.replace(/(<([^>]+)>)/ig, ""));
 	});
 
-	socket.on("typing", function(typing){
+	socket.on("typing", function (typing) {
 		// Only logged in users
-		if(nick != null){
+		if (nick != null) {
 			socket.broadcast.to("main").emit("typing", {
 				status: typing,
 				nick: nick
 			});
 
-			console.log("%s %s typing.", nick.replace(/(<([^>]+)>)/ig,""), typing ? "is" : "is not");
+			console.log("%s %s typing.", nick.replace(/(<([^>]+)>)/ig, ""), typing ? "is" : "is not");
 		}
 	});
 
-	socket.on("disconnect", function() {
+	socket.on("disconnect", function () {
 		console.log("Got disconnect!");
 
-		if(nick != null){
+		if (nick != null) {
 			// Remove user from users
 			users.splice(users.indexOf(nick), 1);
 
@@ -97,7 +112,7 @@ io.sockets.on("connection", function(socket) {
 				"nick": nick
 			});
 
-			console.log("User %s left.", nick.replace(/(<([^>]+)>)/ig,""));
+			console.log("User %s left.", nick.replace(/(<([^>]+)>)/ig, ""));
 			socket.leave("main");
 			nick = null;
 		}
